ecfc0ddf21a6c85890e20953d8fbce3140501271
MD MAFUJUL HASAN <128472454+MD-MAFUJUL-HASAN@users.noreply.github.com>
2023-05-05 15:51:21 +0600

    Add files via upload
    Signed-off-by: MD MAFUJUL HASAN <128472454+MD-MAFUJUL-HASAN@users.noreply.github.com>

---
+++ 100 - The 3n + 1 problem.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int collatz(int n) {
+	int i = 1;
+	while (n > 1) {
+		if (n % 2) n = n * 3 + 1;
+		else n >>= 1;
+		i++;
+	}
+	return i;
+}
+
+int main() {
+	int a, b;
+	while (cin >> a >> b) {
+		int m = 0;
+		for (int i = min(a, b); i <= max(a, b); i++) m = max(m, collatz(i));
+		cout << a << " " << b << " " << m << endl;
+	}
+}
